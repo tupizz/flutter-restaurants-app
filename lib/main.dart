2f16a69eaa8d6e0b15056b22981ea8179bf651cd
Tadeu <tadeu.tupiz@gmail.com>
2019-10-19 00:41:25 -0300

    🌠 Criacao de temas e definicao de cores&fonts

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,30 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Refeições',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.indigo,
+        accentColor: Colors.blue,
+        fontFamily: 'Raleway',
+        canvasColor: Color.fromRGBO(50, 0, 100, 0.3),
+        textTheme: ThemeData.light().textTheme.copyWith(
+              body1: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              body2: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              title: TextStyle(
+                  fontSize: 20,
+                  fontFamily: 'RobotoCondensed',
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                  shadows: [
+                    Shadow(
+                      color: Color.fromRGBO(50, 0, 100, 0.35),
+                      blurRadius: 10,
+                      offset: Offset(2, 2),
+                    ),
+                  ]),
+            ),
       ),
       home: CategoriesPage(),
       debugShowCheckedModeBanner: false,
